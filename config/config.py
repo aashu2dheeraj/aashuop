506d27dca1ad645169135676e1cf21297d63430f
aashu2dheeraj <116055984+aashu2dheeraj@users.noreply.github.com>
2023-04-04 14:26:38 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Xdarnav/ArnavXMusic"
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ilexupdates")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/link_copied")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/mystudytownaashusir1")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/mystudytownaashusir")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
